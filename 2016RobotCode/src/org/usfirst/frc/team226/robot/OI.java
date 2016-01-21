c622eb60577d6124514a4e8f7be8cca72251546c
Alex Jalkanen <AlexJalkanen@gmail.com>
2016-01-21 14:11:10 -0500

    test commit

---
+++ 2016RobotCode/src/org/usfirst/frc/team226/robot/OI.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team226.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	
+	Joystick driver = new Joystick(0);
+	//Joystick manip = new Joystick(1);
+	//Button M1 = new JoystickButton(manip, 1);
+	//Button M2 = new JoystickButton(manip, 2);
+	//Button M3 = new JoystickButton(manip, 3);
+	//Button M4 = new JoystickButton(manip, 4);
+	
+	
+	public double getLeftDriveSpeed() {
+		return driver.getY();
+	}
+	
+	public double getRightDriveSpeed() {
+		return driver.getRawAxis(5);
+	}
+	
+}
+
