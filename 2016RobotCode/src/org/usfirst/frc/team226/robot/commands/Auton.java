a757ba7b4601f80cfe90c388dc00c15a66601220
Alex Jalkanen <AlexJalkanen@gmail.com>
2016-03-20 13:44:55 -0400

    End of Midland Competition

--- 2016RobotCode/src/org/usfirst/frc/team226/robot/commands/Auton.java
+++ 2016RobotCode/src/org/usfirst/frc/team226/robot/commands/Auton.java
@@ -18,40 +18,25 @@ public class Auton extends CommandGroup {
     	addSequential(new DriveWithAuton(2.8, .75, .75));
     	addSequential(new MoveFourBarEncoderReset());
     	
-//    	AutoPop
-    		addSequential(new ShooterWheelsBackward(.25));
-    		addSequential(new IntakeWheelsBackward(.25));
-    		addSequential(new IntakeWheelsDoNothing());
-    		addSequential(new ShooterWheelsDoNothing());
-    	addParallel(new ShooterWheelsForwardAuton());
+////    	AutoPop
+//    		addSequential(new ShooterWheelsBackward(.25));
+//    		addSequential(new IntakeWheelsBackward(.25));
+//    		addSequential(new IntakeWheelsDoNothing());
+//    		addSequential(new ShooterWheelsDoNothing());
+//    	addParallel(new ShooterWheelsForwardAuton());
     	addSequential(new DriveWithAuton(1.15, .75, -.75));
     	addSequential(new DriveWithAuton(.1, 0, 0));
     	addSequential(new DriveWithAuton(.5, .5, .5));
-    	addSequential(new MoveFourBarAuton(1, .75));
-    	addParallel(new MoveFourBarAuton(2, .3));
-    	addSequential(new IntakeWheelsForward(2));
-    	addSequential(new IntakeWheelsDoNothing());
-    	addSequential(new ShooterWheelsDoNothing());
-    	
-//    	else {
-//    		addSequential(new DriveWithAuton(.5, .75, .75));
-//        	addParallel(new MoveFourBarAuton(2, -.25));
-//        	addSequential(new DriveWithAuton(3.5, .75, .75));
-//        	addSequential(new MoveFourBarEncoderReset());
-//        	addSequential(new MoveFourBarAuton(.1, 0));
-//        	addSequential(new DriveWithAuton(.9, .75, -.75));
-//        	addSequential(new DriveWithAuton(.1, 0, 0));
-//    	}
-    	
+//    	addSequential(new MoveFourBarAuton(1, .75));
+//    	addParallel(new MoveFourBarAuton(2, .3));
+//    	addSequential(new IntakeWheelsForward(2));
+//    	addSequential(new IntakeWheelsDoNothing());
+//    	addSequential(new ShooterWheelsDoNothing());    	
     	
     	//addSequential(new DriveWithEncoder(100, .5, .5));
     	
     	//addSequential(new DriveWithAuton(1.8, .75, -.75));  90 Degree turn
     	
-//    	while (!Robot.driveTrain.isAlignedLeft || !Robot.driveTrain.isAlignedRight) {
-//    		Robot.driveTrain.visionDrive(Robot.centerValue);
-//    		Timer.delay(.5);
-//    	}
         // Add Commands here:
         // e.g. addSequential(new Command1());
         //      addSequential(new Command2());
